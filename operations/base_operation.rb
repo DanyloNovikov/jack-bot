c4f5678d140a0a8dbbc01103044c7606f9e39fb2
Danylo Novikov <danylo.novikov@syndicode.com>
2022-05-15 17:50:48 +0300

    adding architecture

--- operations/base_operation.rb
+++ operations/base_operation.rb
@@ -2,15 +2,30 @@
 
 module Operations
   class BaseOperation
-    attr_reader :body, :message
+    attr_reader :body, :message, :server_request_url
 
     def initialize(bot:, message:)
       @bot = bot
       @message = message
+      @server_request_url = set_server_request_url
     end
 
     def perform
       raise 'must be implement'
+    end
+
+    def success
+      raise 'must be implement'
+    end
+
+    def error
+      raise 'must be implement'
+    end
+
+    private
+
+    def set_server_request_url
+      ENV['BASE_SERVER'] + self.class.to_s.split(':').last.downcase
     end
   end
 end
--- operations/stop.rb
+++ operations/stop.rb
@@ -3,9 +3,35 @@
 module Operations
   class Stop < Operations::BaseOperation
     def perform
+      return success if send_request.success
+
+      error(response: send_request)
+    end
+
+    def success
       @bot.api.send_message(
         chat_id: @message.chat.id,
         text: "Bye, #{@message.from.first_name}"
+      )
+    end
+
+    def error(response:)
+      @bot.api.send_message(
+        chat_id: @message.chat.id,
+        text: ''
+      )
+    end
+
+    private
+
+    def send_request
+      Faraday.post(
+        @server_request_url,
+        {
+          first_name: @message.from.first_name,
+          last_name: @message.from.last_name,
+          chat_id: @message.chat.id
+        }
       )
     end
   end
