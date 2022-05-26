292512806d46a27b0d80e6efca4fd7519b45eb33
Danylo Novikov <danylo.novikov@syndicode.com>
2022-05-26 14:41:27 +0300

    Add search by name

--- operations/name.rb
+++ operations/name.rb
@@ -6,12 +6,52 @@ Dir['./models/*.rb'].each { |file| require_relative "../#{file}" }
 
 module Operations
   class Name < Operations::BaseOperation
-    def perform; end
+    def perform
+      @bot.api.send_message(
+        chat_id: @message.from.id,
+        text: 'Enter the name of what you are looking for. example(oasis):'
+      )
+      @bot.listen do |message|
+        return handle_message(message: message)
+      end
+    end
 
     private
 
-    def success; end
+    def success(answer:)
+      @bot.api.sendPhoto(
+        chat_id: @message.from.id,
+        photo: answer['strDrinkThumb']
+      )
+      @bot.api.send_message(
+        chat_id: @message.from.id,
+        text: Services::TextHandlerCocktail.new.text_for_message(answer: answer)
+      )
+    end
 
-    def error(errors:); end
+    def error(errors:)
+      @bot.api.send_message(
+        chat_id: @message.from.id,
+        text: errors.values.join('\n')
+      )
+    end
+
+    # I know it's terrible, but it's not the code that's terrible, it's the api.
+    def handle_message(message:)
+      answer = send_request(message: message)
+
+      return success(answer: JSON.parse(answer.body)['drinks'].first) unless JSON.parse(answer.body)['drinks'].nil?
+
+      error(errors: { errors: ['Found nothing...'] })
+    end
+
+    def send_request(message:)
+      Faraday.get(
+        "https://www.thecocktaildb.com/api/json/v1/#{ENV['COCKTAILS_KEY']}/search.php",
+        {
+          s: message.text.tr(' ', '_').downcase
+        }
+      )
+    end
   end
 end
